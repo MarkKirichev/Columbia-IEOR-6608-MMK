eb21a6fc55d2464bc1c69ff530fc73b37ac8f4c6
Mark Kirichev <mmk2243@columbia.edu>
2023-11-28 14:00:17 -0500

    general functionality for binary FP finished
    m
    -m
    ff
    
    f
    ff
    
    a
    a
    a
    a
    
    a
    a
    a
    a
    a

--- Gurobi_MIPs_Solver.py
+++ Gurobi_MIPs_Solver.py
@@ -6,7 +6,7 @@ filename = '10teams.mps'
 file_path = file_root + filename
 
 
-def model_to_json(file_path):
+def model_to_json(file_path, output_file_name):
     # Read the model
     model = read(file_path)
 
@@ -16,21 +16,39 @@ def model_to_json(file_path):
 
     # Extract constraints
     constraints = []
-    for constr in model.getConstrs():
+    for num_constr, constr in enumerate(model.getConstrs()):
         expr = model.getRow(constr)
-        coeffs = [expr.getCoeff(i) for i in range(expr.size())]
+
+        # print(f"Expression {num_constr+1}: {expr}")
+        total_variables = len(objective)
+
+        # Extracting variable indices from the expression
+        variable_indices = [
+            int(s[1:]) for s in str(expr).split() if s.startswith('x')
+        ]
+
+        # Creating the list with 0's and 1's
+        result_list = [1 if i in variable_indices else 0 for i in range(1, total_variables + 1)]
+
+        print(f"Coefficient {num_constr + 1}: {len(result_list)}")
+
         sense = constr.getAttr("Sense")
+        print(sense)
         rhs = constr.getAttr("RHS")
 
         # Convert Gurobi sense to 'ge' or 'le'
         if sense == GRB.LESS_EQUAL:
-            constraint = {"eq": coeffs, "le": rhs}
+            constraint = {"eq": result_list, "le": rhs}
         elif sense == GRB.GREATER_EQUAL:
-            constraint = {"eq": coeffs, "ge": rhs}
+            constraint = {"eq": result_list, "ge": rhs}
         else:
-            continue  # Skip if it's not a 'ge' or 'le' constraint
+            constraint = {"eq": result_list, "le": rhs} # We'll call the EQ equations both LEQ and GEQ equations for now.
+            # continue  # Skip if it's not a 'ge' or 'le' constraint
+            constraints.append(constraint)
+            constraint = {"eq": result_list, "ge": rhs}
 
         constraints.append(constraint)
+        print(len(constraints))
 
     # Construct JSON
     data = {
@@ -38,11 +56,10 @@ def model_to_json(file_path):
         "constrs": constraints
     }
 
-    # with open('Test_10teams.json', 'w') as f:
-    #     json.dump(data, f, indent=4)
+    with open(f'{output_file_name}.json', 'w') as f:
+        json.dump(data, f, indent=4)
 
     return json.dumps(data, indent=4)
-
 
 
 def solve_mip_model(file_path):
@@ -66,8 +83,22 @@ def solve_mip_model(file_path):
         print(f"Optimization was stopped with status {model.status}")
 
 
-# Replace with the path to your file
-json_data = model_to_json(file_path)
-print(json_data)
+def gurobi_reader(file_path):
 
-solve_mip_model(file_path)
+    model = read(file_path)
+
+    constrs = model.getConstrs()
+
+    for i in range(1, len(constrs) + 1):
+        print(f"Constraint #: {i}")
+        print(model.getRow(constrs[i-1]))
+
+
+# Replace with the path to your file
+# json_data = model_to_json(file_path)
+
+# print(json_data)
+# solve_mip_model(file_path)
+
+model_to_json(file_path=file_path,
+              output_file_name='Test_10teams')
---
+++ test.py
@@ -0,0 +1,5 @@
+x = [
+    (mul, (l, p)) for l, mul in enumerate([10, 3, 2, 1]) for p in [-1, 1]
+]
+
+print(x)
